1acb2f0fe07217092af6f71366405662b0e0a4c3
TonyADI <35707107+TonyADI@users.noreply.github.com>
2021-06-18 16:53:41 -0400

    Fixed responder styling

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -2,17 +2,19 @@ import './Message.css';
 
 export const Message = (props) => {
     const responderStyle = {
-        margin: '10px auto 10px 0'
+        margin: '10px auto 10px 0',
+        textAlign: 'left'
     }
     return (
         <div className='message-container' style={props.responder ? responderStyle :
         {}}>
-            {props.responder}
+            {props.responder && <span className="message-responders-name">
+                {props.responder}</span>}
             <div className='message-body' style={props.responder ?
                  {backgroundColor: 'lightgrey', color: '#000'} : {}}>
                 <span>{props.body}</span>
             </div>
-            <span>{props.time}</span>
+            <span className="message-time">{props.time}</span>
         </div>
     )
 }
--- src/components/MessageList/MessageList.tsx
+++ src/components/MessageList/MessageList.tsx
@@ -9,12 +9,12 @@ export interface message {
 const MessageList = (props: {messages:message[]}) => {
     return (
         <div className='messagelist-container'>
-            <Message body={'haha'} time={'1:20pm'}/>
-            <Message body={'ohoh'} responder={'John'} time={'1:22pm'}/>
-            <Message body={'ohoh'} time={'1:24pm'}/>
-            <Message body={'Sheesh this p is longgg'} time={'1:30pm'} responder={'Polo G.'}/>
+            <Message body={'haha'} time={'1:20 pm'}/>
+            <Message body={'ohoh'} responder={'John'} time={'1:22 pm'}/>
+            <Message body={'ohoh'} time={'1:24 pm'}/>
+            <Message body={'Sheesh this p is longgg'} time={'1:30 pm'} responder={'Polo G.'}/>
             <Message body={`Very very very very very very veru veru very longgggggggg very very very very
-            long ass message`} time={'1:33pm'}/>
+            long ass message`} time={'1:33 pm'}/>
         </div>
     )
 }
